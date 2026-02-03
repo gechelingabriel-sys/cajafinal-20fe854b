9c4734a64e78087b64072d8de86f753d5c97264c
gechelingabriel-sys <gechelingabriel@gmail.com>
2026-02-03 09:30:55 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,7 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
-      <Sonner />
-      <BrowserRouter basename={import.meta.env.BASE_URL.replace(/\/$/, "")}>          <Route path="/" element={<Index />} />
+      <Sonner /> <BrowserRouter basename={import.meta.env.BASE_URL.replace(/\/$/, "")}>          <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
